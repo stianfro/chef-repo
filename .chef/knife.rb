1124555af872a26ec2c3a064a0e1e294aafcbf40
Stian Frøystein <stianfroy@gmail.com>
2017-04-01 13:02:37 +0200

    fixed knife

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,8 +3,8 @@ log_level                :info
 log_location             STDOUT
 node_name                "stianfro"
 client_key               "#{current_dir}/stianfro.pem"
-validation_client_name   "froychef"
-validation_key           "#{current_dir}/froychef.pem"
+validation_client_name   "froychef-validator"
+validation_key           "#{current_dir}/froychef-validator.pem"
 chef_server_url          "https://centos-fra1.stianfro.com/organizations/froychef"
 syntax_check_cache_path  "#{ENV['HOME']}/.chef/syntaxcache"
 cookbook_path            ["#{current_dir}/../cookbooks"]
